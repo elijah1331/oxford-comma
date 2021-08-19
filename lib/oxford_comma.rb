de6c3704fce98a81489e7e9b7840e7e406a7aced
Elijah <emarcano143@hotmail.com>
2021-08-19 10:02:46 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  
+  if array.size == 2
+    array.join(" and ")
+  elsif array.size > 2
+    last_index = array.pop
+    newarr = array.join(", ")
+    newarr << ", and #{last_index}"
+  else
+    array.join
+  end
 end
